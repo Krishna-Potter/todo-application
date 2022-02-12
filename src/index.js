ea91cb9b4660742479559d6586c45c379136fa94
Krishna-Potter <krishna.vebbox@gmail.com>
2022-02-12 05:53:13 +0530

    add task using h2 element

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import "./styles/index.scss";
 
 const body = document.body;
-const div = document.createElement("div");
-body.appendChild(div);
 
-const textBox = document.createElement("input");
-textBox.setAttribute("id", "getTaskBox");
-div.appendChild(textBox);
+const addTask = () => {
+  let getTask = document.getElementById("getTaskBox").value;
+  const headTag = document.createElement("h2");
+  body.appendChild(headTag);
+  headTag.innerHTML = getTask;
+};
 
-const button = document.createElement("button");
-button.innerHTML = "add task";
-button.setAttribute("onclick", "addTask()");
-div.appendChild(button);
+document.getElementById("addTaskBtn").addEventListener("click", addTask);
